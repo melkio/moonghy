bd80ab7bde55e2fa866aa94c8b3dec0b1aaee816
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2014-03-07 16:54:26 +0100

    setup new core projects

---
+++ source/Moonghy.Test/EventsStreamFixture.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Moonghy.Test
+{
+    [TestFixture]
+    public class EventsStreamFixture
+    {
+        // impostando una policy di snapshotting per ogni commit deve calcolare lo snapshot e persisterlo insieme al commit
+    }
+}
